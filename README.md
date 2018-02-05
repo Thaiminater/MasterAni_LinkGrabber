b8b3bcfa97160d5efda337c5e5c58afde900b2aa
Thomas Schühly <thomas.schuehly@outlook.com>
2018-02-05 17:39:30 +0100

    Updated README and added comment to main script

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # MasterAni_LinkGrabber
+
+I'm running this script on a Raspberry Pi 3.
+
+First you have to install [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/#Download), then you have to download [Dryscrape](https://dryscrape.readthedocs.io/en/latest/installation.html) and install it. 
+
+Then you can run the grabber.py script
+
+```
+python grabber.py URL STARTEPISODE ENDEPISODE HOST
+```
+
+#### Hosts
+
++ google
++ Vidstreaming
++ mp4
+
+You can now paste the Textfile created into JDownloader or any Download Manager you want.
--- grabber.py
+++ grabber.py
@@ -4,48 +4,38 @@ import re
 import sys
 import json
 import time
-if  sys.argv[1] == "?":
-    print("python grabber.py URL STARTEPISODE ENDEPISODE HOST")
-    sys.exit(0)
-dryscrape.start_xvfb()
+
+if  sys.argv[1] == "?" or sys.argv[1] == "" :			# Show scheme if no parameters are given
+	print("python grabber.py URL STARTEPISODE ENDEPISODE HOST")
+	sys.exit(0)
+dryscrape.start_xvfb()									# Start dryscrape session
 session = dryscrape.Session()
 start =  int(sys.argv[2])
 stop = int(sys.argv[3])+ 1
 
 
-for i in range(start, stop):
+for i in range(start, stop):							# Create URL for each Episode
     zahl = str(i)
     my_url = sys.argv[1] + "/" + zahl
-    #print(my_url)
-    session.visit(my_url)
+    session.visit(my_url)								# Visit the URL and make it into a Soup
     response = session.body()
     soup = BeautifulSoup(response)
-    #print(soup)
-    mirstr = str(soup.find_all('script'))
+    mirstr = str(soup.find_all('script'))				# Find the part where the download URL is located 
     mirstr = mirstr.split("var args = ",1)[1]
     mirstr = mirstr.split("] }",)[0]
     mirstr = mirstr + "] }"
-    #print(str)
     mirstr = mirstr.replace("anime","\"anime\"")
     mirstr = mirstr.replace("mirrors","\"mirrors\"")
     mirstr = mirstr.replace("auto_update","\"auto_update\"")
-    #print(mirstr)
-    mirdic = json.loads(mirstr)
-    #print(type(mirdic))
+    mirdic = json.loads(mirstr)							# Decode the JSON string into somethin more readable
     embed_id = ""
     hostavail = ""
     prefhost = sys.argv[4]
-    #print(prefhost)
     title = mirdic['anime']['info']['title']
     downurl = "Fehler"
-    # for data in mirdic['anime']:
-    #     #title = data['info']['title']
-    #     print(data['title'])
     for data in mirdic['mirrors']:
         hostname = data['host']['name']
-        #print(hostname)
         qual = data['quality']
-        #print(qual)
         if prefhost == "google" or prefhost == "Google":
             if  hostname == "Drive.g" and qual == 1080:
                 embed_id = data['embed_id']
@@ -84,28 +74,7 @@ for i in range(start, stop):
     print title
     title = title+"_" + str(start) +"-"+ str(stop) + ".txt"
     print(i)
-    # if  downurl == "Fehler":
-    #     for data in mirdic['mirrors']:
-    #         hostname = data['host']['name']
-    #         print(hostname)
-    #         qual = data['quality']
-    #         print(qual)
-    #         if prefhost == "mp4":
-    #             if  hostname == "MP4Upload" and qual == "720":
-    #                 embed_id = data['embed_id']
-    #                 print(embed_id)
-    #                 hostavail = "EP " + str(i) + ": 720p MP4Upload available"
-    #                 downurl = "https://mp4upload.com/" + str(embed_id)
-    # print(hostavail)
-    # print(downurl)
-    #print (mirdic['mirrors']['embed_id'])
-    #print(len(mirdic))
-        # for i in range(0, len(my_string)):
-        # if "\"host_id\":12" in my_string[i] and "\"quality\":1080" in my_string[i +2]:
-        # k = i+1
-        # break
-        # print(my_string[k])
-    text_file = open(title, "a")
+    text_file = open(title, "a")						# Write all the download links into a .txt file
     text_file.write(hostavail + "\n")
     text_file.write(downurl + "\n")
     text_file.close()
